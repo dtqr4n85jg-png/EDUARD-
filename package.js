d041dbec1e7f3fe70b73c59c54c3ab51ca7c475a
dtqr4n85jg-png <dtqr4n85jg@privaterelay.appleid.com>
2025-10-29 17:02:32 +0100

    Add files via upload

---
+++ package.js
@@ -0,0 +1,17 @@
+{
+  "name": "Cosa-Nostra-bot-v2",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "Eduard | AlZein",
+  "license": "ISC",
+  "description": "Discord bot",
+  "dependencies": {
+    "discord.js": "^14.24.1",
+    "dotenv": "^17.2.3"
+    "express":"^4.18.2"
+}
